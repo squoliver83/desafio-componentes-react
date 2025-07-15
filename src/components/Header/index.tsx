220ea5bc8d170fa4c254121437c8e541df95a794
Samuel Quaresma <squoliver@gmail.com>
2025-07-15 12:46:42 -0300

    Footer tomando forma, sessões da página pré-estruturadas, mais CSS

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Header() {
+    return (
+        <header>
+            <div className="dcr-container">
+                <h1>DSCarros Top</h1>
+            </div>
+        </header>
+    );
+}
---
+++ src/routes/Comments/index.tsx
@@ -0,0 +1,30 @@
+import './styles.css';
+import Footer from "../../components/Footer";
+import Header from "../../components/Header"
+
+export default function Comments() {
+    return (
+        <>
+            <Header />
+            <main className="dcr-comments-main">
+                <section id="dcr-comments-visit-section" className="dcr-container">
+                    <div className="dcr-comments-visit-title">
+                        <h2>Venha nos visitar</h2>
+                    </div>
+                    <div className="dcr-comments-visit-cards">
+
+                    </div>
+                </section>
+                <section id="dcr-comments-opinions-section" className="dcr-container">
+                    <div className="dcr-comments-opinions-title">
+                        <h2>O que estão dizendo</h2>
+                    </div>
+                    <div className="dcr-comments-opinions-cards">
+
+                    </div>                    
+                </section>
+            </main>
+            <Footer />
+        </>
+    );
+}
